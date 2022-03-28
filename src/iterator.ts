0b1692eab284435e48a86a4dba4422d646635d9e
Banou <me@banou.dev>
2022-03-28 02:03:53 -0400

    chore: 🔥comment unused code

--- src/iterator.ts
+++ src/iterator.ts
@@ -1,15 +1,15 @@
-import type { ApiResolverOptions } from '..'
+// import type { ApiResolverOptions } from '..'
 
-export default
-  func =>
-    async ({ port, ...rest }: ApiResolverOptions) => {
-      port.start()
-      for await (const value of func({ port, ...rest })) {
-        if (Array.isArray(value)) {
-          port.postMessage(value[0], <Transferable[]>value[1])
-        } else {
-          port.postMessage(value)
-        }
-      }
-      port.close()
-    }
+// export default
+//   func =>
+//     async ({ port, ...rest }: ApiResolverOptions) => {
+//       port.start()
+//       for await (const value of func({ port, ...rest })) {
+//         if (Array.isArray(value)) {
+//           port.postMessage(value[0], <Transferable[]>value[1])
+//         } else {
+//           port.postMessage(value)
+//         }
+//       }
+//       port.close()
+//     }
--- src/pull.ts
+++ src/pull.ts
@@ -1,42 +1,42 @@
-import type { Resolvers } from '@mfkn/fkn-web'
+// import type { Resolvers } from '@mfkn/fkn-web'
 
-type pull = (
-  type: keyof Resolvers,
-  data?: any,
-  transfer?: Transferable[]
-) => AsyncGenerator
+// type pull = (
+//   type: keyof Resolvers,
+//   data?: any,
+//   transfer?: Transferable[]
+// ) => AsyncGenerator
 
-const pull: pull = async function * (type, data, transfer = []) {
-  const { port1, port2 } = new MessageChannel()
+// const pull: pull = async function * (type, data, transfer = []) {
+//   const { port1, port2 } = new MessageChannel()
 
-  window.parent.postMessage(
-    {
-      source: 'oz-package-api',
-      type,
-      data,
-      port: port2
-    },
-    '*',
-    [port2, ...transfer]
-  )
+//   window.parent.postMessage(
+//     {
+//       source: 'oz-package-api',
+//       type,
+//       data,
+//       port: port2
+//     },
+//     '*',
+//     [port2, ...transfer]
+//   )
 
-  let done = false
-  let resolve
-  let currentPromise = new Promise(_resolve => resolve = _resolve)
-  port1.addEventListener(
-    'message',
-    ({ data: { value, done: _done } }) => {
-      if (_done) done = true
-      resolve(value)
-    }
-  )
-  port1.start()
-  while (!done) {
-    yield await currentPromise
-    currentPromise = new Promise(_resolve => resolve = _resolve)
-  }
-  port1.close()
-  port2.close()
-}
+//   let done = false
+//   let resolve
+//   let currentPromise = new Promise(_resolve => resolve = _resolve)
+//   port1.addEventListener(
+//     'message',
+//     ({ data: { value, done: _done } }) => {
+//       if (_done) done = true
+//       resolve(value)
+//     }
+//   )
+//   port1.start()
+//   while (!done) {
+//     yield await currentPromise
+//     currentPromise = new Promise(_resolve => resolve = _resolve)
+//   }
+//   port1.close()
+//   port2.close()
+// }
 
-export default pull
+// export default pull
