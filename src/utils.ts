a15bda2d595da97ff55153207e5b791ce0166c75
Banou <me@banou.dev>
2022-03-28 02:06:15 -0400

    refactor: ♻️replace TypedArray support by ArrayBuffer

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,8 @@
 
 
-export type TypedArray = Int8Array | Uint8Array | Uint8ClampedArray | Int16Array | Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array | BigInt64Array | BigUint64Array
 
 export type TransferableObject = ArrayBuffer | MessagePort | ReadableStream | WritableStream | TransformStream | /* AudioData | */ ImageBitmap /* | VideoFrame | OffscreenCanvas */
 
-const typedArrays = [Int8Array, Uint8Array, Uint8ClampedArray, Int16Array, Uint16Array, Int32Array, Uint32Array, Float32Array, Float64Array, BigInt64Array, BigUint64Array]
 
 const isTransferable = (value: any) =>
   value instanceof ArrayBuffer ? true :
@@ -13,7 +11,6 @@ const isTransferable = (value: any) =>
   value instanceof WritableStream ? true :
   value instanceof TransformStream ? true :
   value instanceof ImageBitmap ? true :
-  typedArrays.some(type => value instanceof type) ? true :
   false
 
 export const getTransferableObjects = (value: any): TransferableObject[] => {
