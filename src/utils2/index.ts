239713632d7d1688497a594c335a1788a897785e
Banou <me@banou.dev>
2022-03-28 02:03:31 -0400

    chore: 🔥remove unused code

--- src/utils2/index.ts
+++
@@ -1,5 +0,0 @@
-export type { Await } from './types'
-export { default as makeEventTarget } from './event-target'
-export { default as makePromise } from './promise'
-export { default as makeIterator } from './iterator'
-
