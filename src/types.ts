52ee545d6e38258247b6515b740a898ff0ecccf4
Banou <me@banou.dev>
2022-09-22 05:53:47 +0200

    fix: ✏️typo Structrured to Structured & improve types

--- src/types.ts
+++ src/types.ts
@@ -2,25 +2,25 @@ export type TransferableObject =
   ArrayBuffer | MessagePort | ReadableStream | WritableStream |
   TransformStream | /* AudioData | */ ImageBitmap /* | VideoFrame | OffscreenCanvas */
 
-export interface StructuredCloneObject {
-  [key: keyof any]: StructuredCloneType
+export type StructuredCloneObject = {
+  [key: string]: StructuredCloneType
 }
 
 export type StructuredCloneType =
   boolean | null | undefined | number | BigInt | string | Date | RegExp | Blob | File | FileList | ArrayBuffer | ArrayBufferView |
   ImageBitmap | ImageData | Array<StructuredCloneType> | StructuredCloneObject | Map<StructuredCloneType, StructuredCloneType> | Set<StructuredCloneType>
 
-export interface StructuredCloneTransferableObject {
-  [key: keyof any]: StructruredCloneTransferableType
+export type StructuredCloneTransferableObject = {
+  [key: string]: StructuredCloneTransferableType
 }
 
-export type StructruredCloneTransferableType =
-  StructuredCloneType | TransferableObject | Array<StructruredCloneTransferableType> | StructuredCloneTransferableObject |
-  Map<StructruredCloneTransferableType, StructruredCloneTransferableType> | Set<StructruredCloneTransferableType>
+export type StructuredCloneTransferableType =
+  StructuredCloneType | TransferableObject | Array<StructuredCloneTransferableType> | StructuredCloneTransferableObject |
+  Map<StructuredCloneTransferableType, StructuredCloneTransferableType> | Set<StructuredCloneTransferableType>
 
 export type Target = Window | ServiceWorker | Worker
 
-export type Resolver = (data: StructruredCloneTransferableType, extra: ApiResolverOptions) => any
+export type Resolver = (data: StructuredCloneTransferableType, extra: ApiResolverOptions) => any
 
 export type Resolvers = {
   [key: string]: Resolver
