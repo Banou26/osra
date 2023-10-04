fb147ba4a1bfadc071d731e2858d694e58828f8e
Banou <me@banou.dev>
2023-10-05 00:58:51 +0200

    feat: ✨add proxyTarget to register function

--- src/register.ts
+++ src/register.ts
@@ -1,24 +1,45 @@
-import type { ApiMessageData, ApiResolverOptions, Resolvers, ValidateResolvers } from './types'
+import type { ApiMessageData, ApiResolverOptions, Resolvers, Target, ValidateResolvers } from './types'
 
 import { MESSAGE_SOURCE_KEY } from './shared'
+import { getTransferableObjects } from './utils'
 
 export const registerListener = <T extends Record<PropertyKey, (data: any, extra: ApiResolverOptions) => unknown>>({
   target,
   resolvers,
   filter,
   map,
-  key = MESSAGE_SOURCE_KEY
+  key = MESSAGE_SOURCE_KEY,
+  proxyTarget
 }: {
   target: WindowEventHandlers | ServiceWorkerContainer | Worker | SharedWorker
   resolvers: ValidateResolvers<T>
   filter?: (event: MessageEvent<any>) => boolean
   map?: (...args: Parameters<Resolvers[string]>) => Parameters<Resolvers[string]>
-  key?: string
+  key?: string,
+  proxyTarget?: Target
 }) => {
   const listener = (event: MessageEvent<ApiMessageData<Resolvers>>) => {
     if (!event.data || typeof event.data !== 'object') return
     if (event.data?.source !== key) return
     if (filter && !filter(event)) return
+
+    if (proxyTarget) {
+      const { type, data, port } = event.data
+      const transferables = getTransferableObjects(data)
+      proxyTarget.postMessage(
+        {
+          source: key,
+          type,
+          data,
+          port
+        },
+        {
+          targetOrigin: '*',
+          transfer: [port, ...transferables as unknown as Transferable[] ?? []]
+        }
+      )
+      return
+    }
 
     const { type, data, port } = event.data
     const resolver = resolvers[type]
