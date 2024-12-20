3d986874b947d6bc7ec5387a72a500a6d8d1b7ae
Banou <me@banou.dev>
2024-12-20 06:35:39 +0100

    refactor: ♻️improve test setup

--- tests/index.ts
+++ tests/index.ts
@@ -1,20 +1,33 @@
+import { use, expect } from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+
 import { call, makeCallListener } from '../src/call'
 import { registerListener } from '../src/register'
 
+use(chaiAsPromised)
+
 export const test1 = async () => {
-  registerListener({
+  const listener = makeCallListener(async (data: { foo: number }, bar: string) => {
+    if (data.foo !== 1) {
+      throw new Error('foo is not 1')
+    }
+    if (bar !== 'bar') {
+      throw new Error('bar is not bar')
+    }
+    return 1
+  })
+
+  const { resolvers } = registerListener({
     target: window,
     resolvers: {
-      'test': makeCallListener(async (data: { foo: number }) => {
-        if (data.foo !== 1) {
-          throw new Error('foo is not 1')
-        }
-        return 1
-      })
+      test: listener
     }
   })
 
-  await call(window)('test', { foo: 1 })
+  const callFunc = call<typeof resolvers>(window)
+
+  await expect(callFunc('test', { foo: 1 }, 'bar')).to.eventually.equal(1)
+  await expect(callFunc('test', { foo: 0 }, 'ba')).to.be.rejected
 }
 
 export const describe1 = {
