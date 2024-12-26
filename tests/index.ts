6b80caf397b5387c36eeb11f444c9f4c928c8ebf
Banou <me@banou.dev>
2024-12-26 01:54:02 +0100

    refactor: 🚧cleanup & fix issues

--- tests/index.ts
+++ tests/index.ts
@@ -1,7 +1,7 @@
 import { use, expect } from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 
-import { expose } from '../src/register'
+import { expose } from '../src/index'
 
 use(chaiAsPromised)
 
@@ -18,9 +18,9 @@ export const baseArgsAndResponse = async () => {
         return 1
       }
     },
-    { target: window, messageListener: window }
+    { remote: window, local: window }
   )
-  const { test } = await expose({}, { target: window, messageListener: window })
+  const { test } = await expose({}, { remote: window, local: window })
 
   await expect(test({ foo: 1 }, 'bar')).to.eventually.equal(1)
   await expect(test({ foo: 0 }, 'baz')).to.be.rejected
@@ -29,9 +29,9 @@ export const baseArgsAndResponse = async () => {
 export const callback = async () => {
   expose(
     { test: () => () => 1 },
-    { target: window, messageListener: window }
+    { remote: window, local: window }
   )
-  const { test } = await expose({}, { target: window, messageListener: window })
+  const { test } = await expose({}, { remote: window, local: window })
 
   const result = await test()
   await expect(result()).to.eventually.equal(1)
@@ -40,9 +40,9 @@ export const callback = async () => {
 export const callbackAsArg = async () => {
   expose(
     { test: async (callback: () => number) => callback() },
-    { target: window, messageListener: window }
+    { remote: window, local: window }
   )
-  const { test } = await expose({}, { target: window, messageListener: window })
+  const { test } = await expose({}, { remote: window, local: window })
 
   const result = await test(() => 1)
   await expect(result).to.equal(1)
@@ -51,9 +51,9 @@ export const callbackAsArg = async () => {
 export const polyfilledMessageChannel = async () => {
   expose(
     { test: async (callback: () => number) => callback() },
-    { target: window, messageListener: window }
+    { remote: window, local: window }
   )
-  const { test } = await expose({}, { target: window, messageListener: window })
+  const { test } = await expose({}, { remote: window, local: window })
 
   const result = await test(() => 1)
   await expect(result).to.equal(1)
