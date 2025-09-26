4288376db08bed27e8516a8a80336c856db5653c
Banou <me@banou.dev>
2025-09-27 00:33:49 +0900

    refactor: ♻️make playwright work with arch

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,18 +3,23 @@ import { defineConfig, devices } from '@playwright/test'
 export default defineConfig({
   timeout: 5000,
   fullyParallel: true,
+  use: {
+    connectOptions: {
+      wsEndpoint: 'ws://127.0.0.1:8010/'
+    }
+  },
   projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
+    // {
+    //   name: 'chromium',
+    //   use: { ...devices['Desktop Chrome'] },
+    // },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
   ]
 })
