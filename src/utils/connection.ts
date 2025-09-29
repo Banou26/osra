bbf59befe4214a02fb9493bf4fcf2382449ea1a4
Banou <me@banou.dev>
2025-09-30 05:45:27 +0900

    refactor: ♻️start setting up connections

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -1,7 +1,7 @@
-import type { Message, MessageContext } from '../types'
+import type { Capable, Message, MessageContext } from '../types'
 import type { PlatformCapabilities } from './capabilities'
 
-export const startConnection = (
+export const startBidirectionalConnection = (
   { uuid, remoteUuid, platformCapabilities, send, close }:
   {
     uuid: string
@@ -17,4 +17,47 @@ export const startConnection = (
   }
 }
 
-export type Connection = ReturnType<typeof startConnection>
+export type BidirectionalConnection = ReturnType<typeof startBidirectionalConnection>
+
+export const startUnidirectionalEmittingConnection = <T extends Capable>(
+  { value, uuid, platformCapabilities, send, close }:
+  {
+    value: Capable
+    uuid: string
+    platformCapabilities: PlatformCapabilities
+    send: (message: Message) => void
+    close: () => void
+  }
+) => {
+
+  return {
+    proxy: new Proxy(
+      new Function(),
+      {
+        apply: (target, thisArg, args) => {
+        },
+        get: (target, prop) => {
+        }
+      }
+    ) as T
+  }
+}
+
+export type UnidirectionalEmittingConnection = ReturnType<typeof startUnidirectionalEmittingConnection>
+
+export const startUnidirectionalReceivingConnection = (
+  { uuid, remoteUuid, platformCapabilities, close }:
+  {
+    uuid: string
+    remoteUuid?: string
+    platformCapabilities: PlatformCapabilities
+    close: () => void
+  }
+) => {
+  return {
+    receiveMessage: (message: Message, messageContext: MessageContext) => {
+    }
+  }
+}
+
+export type UnidirectionalReceivingConnection = ReturnType<typeof startUnidirectionalReceivingConnection>
