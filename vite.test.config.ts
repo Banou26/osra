24238933779c214b879adcd32c960b4ef9f1418b
Banou <me@banou.dev>
2025-10-27 03:17:26 +0900

    refactor: ♻️add code coverage

--- vite.test.config.ts
+++ vite.test.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
+import istanbul from 'vite-plugin-istanbul'
 
 export default defineConfig({
+  plugins: [
+    istanbul({
+      include: 'src/*',
+      exclude: ['node_modules', 'tests/'],
+      extension: ['.js', '.ts'],
+      requireEnv: false,
+      forceBuildInstrument: true
+    })
+  ],
   build: {
     target: 'esnext',
     outDir: 'build',
