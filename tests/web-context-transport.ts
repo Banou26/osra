29b44ca9f9a89086d73ab365f2efc925560ecfc2
Banou <me@banou.dev>
2025-10-27 03:02:59 +0900

    refactor: ♻️fix certain broken revive cases

--- tests/web-context-transport.ts
+++ tests/web-context-transport.ts
@@ -20,45 +20,66 @@ export const baseArgsAndResponse = async () => {
   await expect(test({ foo: 0 }, 'baz')).to.be.rejected
 }
 
+export const callback = async () => {
+  const value = async () => async () => 1
+  expose(value, { transport: window })
 
-// export const baseArgsAndResponse = async () => {
-//   const value = {
-//     test: async (data: { foo: number }, bar: string) => {
-//       if (data.foo !== 1) {
-//         throw new Error('foo is not 1')
-//       }
-//       if (bar !== 'bar') {
-//         throw new Error('bar is not bar')
-//       }
-//       return 1
-//     }
-//   }
-//   expose(value, { transport: window })
-//   console.log('1')
+  const test = await expose<typeof value>({}, { transport: window })
 
-//   const { test } = await expose<typeof value>({}, { transport: window })
-//   console.log('2')
+  const result = await test()
+  await expect(result()).to.eventually.equal(1)
+}
 
-//   await expect(test({ foo: 1 }, 'bar')).to.eventually.equal(1)
-//   await expect(test({ foo: 0 }, 'baz')).to.be.rejected
-// }
+export const callbackAsArg = async () => {
+  const value = async (callback: () => number) => callback()
+  expose(value, { transport: window })
 
-// export const callback = async () => {
-//   const value = { test: async () => async () => 1 }
-//   expose(value, { transport: window })
+  const test = await expose<typeof value>({}, { transport: window })
 
-//   const { test } = await expose<typeof value>({}, { transport: window })
+  const result = await test(() => 1)
+  expect(result).to.equal(1)
+}
 
-//   const result = await test()
-//   await expect(result()).to.eventually.equal(1)
-// }
+export const objectBaseArgsAndResponse = async () => {
+  const value = {
+    test: async (data: { foo: number }, bar: string) => {
+      if (data.foo !== 1) {
+        throw new Error('foo is not 1')
+      }
+      if (bar !== 'bar') {
+        throw new Error('bar is not bar')
+      }
+      return 1
+    }
+  }
+  expose(value, { transport: window })
 
-// export const callbackAsArg = async () => {
-//   const value = { test: async (callback: () => number) => callback() }
-//   expose(value, { transport: window })
+  const { test } = await expose<typeof value>({}, { transport: window })
 
-//   const { test } = await expose<typeof value>({}, { transport: window })
+  await expect(test({ foo: 1 }, 'bar')).to.eventually.equal(1)
+  await expect(test({ foo: 0 }, 'baz')).to.be.rejected
+}
 
-//   const result = await test(() => 1)
-//   expect(result).to.equal(1)
-// }
+export const objectCallback = async () => {
+  const value = {
+    test: async () => async () => 1
+  }
+  expose(value, { transport: window })
+
+  const { test } = await expose<typeof value>({}, { transport: window })
+
+  const result = await test()
+  await expect(result()).to.eventually.equal(1)
+}
+
+export const objectCallbackAsArg = async () => {
+  const value = {
+    test: async (callback: () => number) => callback()
+  }
+  expose(value, { transport: window })
+
+  const { test } = await expose<typeof value>({}, { transport: window })
+
+  const result = await test(() => 1)
+  expect(result).to.equal(1)
+}
