ce2355d1dd77aacdd92e9cff460a50847bcd0cfa
Banou <me@banou.dev>
2025-12-19 02:05:39 +0900

    refactor: ♻️fix typing issues

---
+++ src/uint8array-base64.d.ts
@@ -0,0 +1,10 @@
+// ES2024 Uint8Array Base64 methods
+// https://github.com/tc39/proposal-arraybuffer-base64
+
+interface Uint8Array<TArrayBuffer extends ArrayBufferLike> {
+  toBase64(): string
+}
+
+interface Uint8ArrayConstructor {
+  fromBase64(base64: string): Uint8Array<ArrayBuffer>
+}
